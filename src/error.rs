7fecfa1775d3ed7b591998ddfbd84ae4384536d4
Teddy Heinen <teddy@heinen.dev>
2020-05-22 14:30:06 -0500

    clean things up (a bit) and add a generic error type

---
+++ src/error.rs
@@ -0,0 +1,29 @@
+use nand2tetris_hdl_parser::HDLParseError;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct GenericError {
+    details: String
+}
+
+impl GenericError {
+    fn new(msg: &str) -> GenericError {
+        GenericError { details: msg.to_string() }
+    }
+}
+
+impl From<std::io::Error> for GenericError {
+    fn from(x: std::io::Error) -> Self {
+        GenericError {
+            details: x.to_string()
+        }
+    }
+}
+
+impl From<HDLParseError> for GenericError {
+    fn from(x: HDLParseError) -> Self {
+        GenericError {
+            details: x.to_string()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+mod error;
+
 #[macro_use]
 extern crate cached;
 
 use std::{fs, env};
 use nand2tetris_hdl_parser::{parse_hdl, Chip, HDLParseError, Part};
 use std::collections::{HashSet, HashMap};
-use std::io::Error;
 use std::iter::FromIterator;
 use std::path::Path;
-
+use error::GenericError;
 
 struct Edge {
     source: String,
@@ -15,38 +16,37 @@ struct Edge {
     pin_name: String,
 }
 
-fn resolve(chip: &str) -> Chip {
+fn resolve(chip: &str) -> Result<Chip, GenericError> {
     let filename = &[&chip.split("_").next().unwrap(), ".hdl"].join("");
-    let hdl = match fs::read_to_string(filename) {
-        Ok(f) => f,
-        Err(e) => panic!("Could not read file {:?}: {:?}", filename, e),
-    };
-    parse_hdl(&hdl).unwrap()
+    let hdl = fs::read_to_string(filename)?;
+    Ok(parse_hdl(&hdl)?)
 }
 
-fn generate_graph(filename: &str) -> Vec<Edge> {
-    let chip = resolve(filename);
-    let mut occurences: HashMap<String, u32> = HashMap::new();
-    let parts = chip.parts
-        .iter()
-        .enumerate()
-        .map(|(x, y)| {
-            let new_name = {
-                if occurences.contains_key(&y.name) {
-                    let count = occurences.entry(y.name.clone()).or_insert(1);
-                    *count += 1;
-                    String::from("_") + &(*count).to_string()
-                } else {
-                    occurences.entry(y.name.clone()).or_insert(1);
-                    String::from("")
+fn generate_graph(filename: &str) -> Result<Vec<Edge>, GenericError> {
+    let chip = resolve(filename)?;
+    let parts = {
+        let mut occurrences: HashMap<String, u32> = HashMap::new();
+        chip.parts
+            .iter()
+            .enumerate()
+            .map(|(x, y)| {
+                let new_name = {
+                    if occurrences.contains_key(&y.name) {
+                        let count = occurrences.entry(y.name.clone()).or_insert(1);
+                        *count += 1;
+                        String::from("_") + &(*count).to_string()
+                    } else {
+                        occurrences.entry(y.name.clone()).or_insert(1);
+                        String::from("")
+                    }
+                };
+                Part {
+                    name: y.clone().name + &new_name,
+                    ..y.clone()
                 }
-            };
-            Part {
-                name: y.clone().name + &new_name,
-                ..y.clone()
-            }
-        })
-        .collect::<Vec<(Part)>>();
+            })
+            .collect::<Vec<(Part)>>()
+    };
     let mut graph: Vec<Edge> = Vec::new();
     let mut connections: HashMap<String, String> = HashMap::new();
     for part in parts {
@@ -66,8 +66,8 @@ fn generate_graph(filename: &str) -> Vec<Edge> {
                 })
             }
             if connections.contains_key(&pin.name) {
-                let conn_chip = resolve(&part.name);
-                let edge = if conn_chip.inputs.iter().any(|x| x.name == pin.name) {
+                let conn_chip = resolve(&part.name)?;
+                graph.push(if conn_chip.inputs.iter().any(|x| x.name == pin.name) {
                     // current chip has pin in inputs so its the destination chip
                     Edge {
                         source: part.name.clone(),
@@ -81,22 +81,21 @@ fn generate_graph(filename: &str) -> Vec<Edge> {
                         dest: part.name.clone(),
                         pin_name: pin.name,
                     }
-                };
-                graph.push(edge)
+                });
             } else {
                 connections.insert(pin.name, part.name.clone());
             }
         }
     }
-    graph
+    Ok(graph)
 }
 
-fn main() -> Result<(), Error> {
-    let path = &env::args().nth(1).unwrap();
-    let file = Path::new(path);
-    env::set_current_dir(file.parent().unwrap());
-    let mut root_chip = file.file_name().unwrap().to_str().unwrap().split(".").next().unwrap();
-    let v = generate_graph(root_chip);
+fn main() -> Result<(), GenericError> {
+    let root_file = &env::args().nth(1).unwrap();
+    let root_file_path = Path::new(root_file);
+    env::set_current_dir(root_file_path.parent().unwrap());
+    let root_chip_name = root_file_path.file_name().unwrap().to_str().unwrap().split(".").next().unwrap();
+    let v = generate_graph(root_chip_name)?;
     println!("digraph {{");
     for edge in v {
         println!("\t{} -> {} [ label=\" {}\" ];", edge.source, edge.dest, edge.pin_name);
